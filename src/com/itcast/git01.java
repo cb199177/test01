f4fa8b38634aca1677f0f514a1439bb7030d37f4
cb199177 <974355655@qq.com>
2019-01-03 18:40:30 +0800

    test

--- src/com/itcast/git01.java
+++ src/com/itcast/git01.java
@@ -2,7 +2,7 @@ package com.itcast;
 
 public class git01 {
     public static void main(String[] args) {
-        System.out.println("defwetete");
-        System.out.println("21423423");
+
+        System.out.println("afwewetgerrtjtyk");
     }
 }
